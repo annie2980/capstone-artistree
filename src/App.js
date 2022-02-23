3b117dc1d775e19d5a7dddbc44e172589e7b0063
ANNIE LIU <annie.liu121@gmail.com>
2022-02-22 17:50:21 -0800

    Add NavigationBar and MUI icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Route, Switch, Redirect, NavLink } from 'react-router-dom';
 import { Navbar, Nav, NavDropdown, Container, Image } from 'react-bootstrap';
-import { AppBar } from '@mui/material'
+import { AppBar, Toolbar, IconButton, BottomNavigation, BottomNavigationAction, Paper } from '@mui/material'
+import { HomeOutlined, SearchOutlined, ChatBubbleOutline, PersonOutline } from '@mui/icons-material'
 
 function App() {
   return (
@@ -13,23 +14,30 @@ function App() {
 
 function NavigationBar() {
   return(
-    // <Nav justify>
-    //   <Nav.Item>
-    //     <img src="img/home-light.png" alt="home icon"/>
-    //   </Nav.Item>
-    //   <Nav.Item>
-    //     <img src="img/home-light.png" alt="home icon"/>
-    //   </Nav.Item>
-    //   <Nav.Item>
-    //     <img src="img/home-light.png" alt="home icon"/>
-    //   </Nav.Item>
-    //   <Nav.Item>
-    //     <img src="img/home-light.png" alt="home icon"/>
-    //   </Nav.Item>
-    // </Nav>
-    <AppBar position="fixed" color="primary">
-
-    </AppBar>
+    // <AppBar position="fixed" color="background" sx={{ top: 'auto', bottom: 0 }}>
+    //   <Toolbar>
+    //     <IconButton>
+    //       <HomeOutlined />
+    //     </IconButton>
+    //     <IconButton>
+    //       <SearchOutlined />
+    //     </IconButton>
+    //     <IconButton>
+    //       <ChatBubbleOutline />
+    //     </IconButton>
+    //     <IconButton>
+    //       <PersonOutline />
+    //     </IconButton>
+    //   </Toolbar>
+    // </AppBar>
+    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+      <BottomNavigation >
+        <BottomNavigationAction label="Home" icon={<HomeOutlined />} />
+        <BottomNavigationAction label="Search" icon={<SearchOutlined />} />
+        <BottomNavigationAction label="Message" icon={<ChatBubbleOutline />} />
+        <BottomNavigationAction label="User" icon={<PersonOutline />} />
+      </BottomNavigation>
+    </Paper>
   )
 }
 
